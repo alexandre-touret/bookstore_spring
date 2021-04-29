37064036194e0c2411cb1cdca12ee09f00152373
Alexandre Touret <alexandre.touret@free.fr>
2021-04-29 17:08:27 +0200

    Add rest numbers skeleton

---
+++ rest-numbers/src/test/java/org/techforum/spring/number/RestNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.techforum.spring.number;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestNumbersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

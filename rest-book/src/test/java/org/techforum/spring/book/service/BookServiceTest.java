999623fad5509c1b5ebbca05864e703e271fb732
Alexandre Touret <alexandre.touret@free.fr>
2021-04-30 16:39:36 +0200

    Circuit breaker implementation

--- rest-book/src/test/java/org/techforum/spring/book/service/BookServiceTest.java
+++ rest-book/src/test/java/org/techforum/spring/book/service/BookServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -34,12 +35,14 @@ class BookServiceTest {
     @MockBean
     private RestTemplate restTemplate;
 
+    @MockBean
+    private CircuitBreakerFactory circuitBreakerFactory;
 
     private ResponseEntity<IsbnNumbers> isbnNumbersResponseEntity;
 
     @BeforeEach
     void setUp() {
-        bookService = new BookService(bookRepository, restTemplate, "URL");
+        bookService = new BookService(bookRepository, restTemplate, "URL", circuitBreakerFactory);
     }
 
     @Test

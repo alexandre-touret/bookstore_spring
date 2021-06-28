55bf65df93aab3526e6856c90732b96d9dfca4f2
Alexandre Touret <alexandre.touret@free.fr>
2021-06-28 16:05:51 +0200

    changing package name

---
+++ rest-number/src/test/java/info/bookstore/spring/number/RestNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package info.bookstore.spring.number;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestNumbersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

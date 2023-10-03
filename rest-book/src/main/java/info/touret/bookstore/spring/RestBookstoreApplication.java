21c4c27492478cf637ec66ddae7856c50ab492a1
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2023-10-03 14:13:18 +0200

    feat: move to spring boot 3.2 & LOOM

--- rest-book/src/main/java/info/touret/bookstore/spring/RestBookstoreApplication.java
+++ rest-book/src/main/java/info/touret/bookstore/spring/RestBookstoreApplication.java
@@ -4,13 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.support.TaskExecutorAdapter;
-
-import java.util.concurrent.Executors;
 
 /**
  * Main Spring boot class
